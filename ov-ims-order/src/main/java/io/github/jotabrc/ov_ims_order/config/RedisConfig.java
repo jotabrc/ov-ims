68d973882f0b1bcd20eb1a55f870e2edf8ac3cc6
jotabrc <ojcroveda@gmail.com>
2025-06-08 15:22:01 -0300

    feat: redis for order kafkaConsumer retryable

---
+++ ov-ims-order/src/main/java/io/github/jotabrc/ov_ims_order/config/RedisConfig.java
@@ -0,0 +1,59 @@
+package io.github.jotabrc.ov_ims_order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName("redis-cache");
+
+        Optional<String> PORT = Optional.ofNullable(System.getenv("REDIS_PORT"));
+        Optional<String> PASSWORD = Optional.ofNullable(System.getenv("REDIS_PASSWORD"));
+
+        redisConfig.setPort(Integer.parseInt(PORT.orElse("6379")));
+        redisConfig.setPassword(PASSWORD.orElse("admin"));
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager config(RedisConnectionFactory factory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
+}
--- ov-ims-order/src/main/java/io/github/jotabrc/ov_ims_order/kafka/KafkaEventConsumer.java
+++ ov-ims-order/src/main/java/io/github/jotabrc/ov_ims_order/kafka/KafkaEventConsumer.java
@@ -2,54 +2,68 @@ package io.github.jotabrc.ov_ims_order.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.jotabrc.ov_ims_order.config.RedisConfig;
 import io.github.jotabrc.ov_ims_order.dto.OrderDtoReservedMessage;
 import io.github.jotabrc.ov_ims_order.dto.OrderUpdateTypeDto;
 import io.github.jotabrc.ov_ims_order.dto.UpdateType;
 import io.github.jotabrc.ov_ims_order.model.OrderStatus;
 import io.github.jotabrc.ov_ims_order.service.OrderService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class KafkaEventConsumer {
 
     private final OrderService orderService;
+    private final RedisConfig redisConfig;
 
     @KafkaListener(topics = {"RESERVE_REPLY"},
             containerFactory = "kafkaListenerContainerFactory")
+    @RetryableTopic(
+            attempts = "5",
+            backoff = @Backoff(delay = 1500, multiplier = 1.5)
+    )
     public void listener(ConsumerRecord<String, String> record) throws JsonProcessingException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            OrderDtoReservedMessage message = objectMapper.readValue(record.value(), OrderDtoReservedMessage.class);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        OrderDtoReservedMessage message = objectMapper.readValue(record.value(), OrderDtoReservedMessage.class);
-
-        buildReaction(message);
+            buildReaction(message);
+        } catch (JsonProcessingException e) {
+            log.error("Message deserialization error: {}", record.value(), e);
+        }
     }
 
     @Async
     private void buildReaction(final OrderDtoReservedMessage message) throws JsonProcessingException {
-        OrderUpdateTypeDto orderUpdateTypeDto;
-        UpdateType type;
-        OrderStatus status;
-
-        if (message.isReserved()) {
-            type = UpdateType.NEW_STATUS;
-            status = OrderStatus.HAS_INVENTORY;
-        } else {
-            type = UpdateType.CANCEL;
-            status = OrderStatus.CANCELLED;
-        }
-
-        orderUpdateTypeDto = OrderUpdateTypeDto
+        if (messageProcessingLimiter(message)) return;
+        OrderUpdateTypeDto dto = OrderUpdateTypeDto
                 .builder()
-                .type(type)
+                .type(message.isReserved() ? UpdateType.NEW_STATUS : UpdateType.CANCEL)
                 .uuid(message.getUuid())
-                .status(status)
+                .status(message.isReserved() ? OrderStatus.HAS_INVENTORY : OrderStatus.CANCELLED)
                 .build();
 
-        orderService.update(orderUpdateTypeDto);
+        orderService.update(dto);
+    }
+
+    private boolean messageProcessingLimiter(OrderDtoReservedMessage message) {
+        Boolean firstAttempt = redisConfig
+                .redisTemplate()
+                .opsForValue()
+                .setIfAbsent(message.getUuid(), message);
+        if (Boolean.FALSE.equals(firstAttempt)) {
+            log.warn("Message already processed for message: {}", message.getUuid());
+            return true;
+        }
+        return false;
     }
 }
